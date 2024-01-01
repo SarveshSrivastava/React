75a653ca7e9a7c90a41ac113099b169904b8d39a
SarveshSrivastava <ssarveshss2997@gmail.com>
2024-01-02 01:59:11 +0530

    added comments for better understanding

--- App.js
+++ App.js
@@ -22,6 +22,12 @@ const parent = React.createElement("div", { id: "parent" },
                 [React.createElement("div", { id: "child1" }, [React.createElement("h1", {},"Child1 H1 tag"), React.createElement("h2", {},"Child1 H2 tag")]), 
                     React.createElement("div", { id: "child2" }, [React.createElement("h1", {},"Child2 H1 tag"), React.createElement("h2", {},"Child2 H2 tag")])])
 
+//React.createElement()  this is an object not an html tag
+//React.createElement(tag, {attributes to the tag like id, name ......}, child element) accepts 3 arguments
+//sibling tags are declared in array
+
+console.log(parent)     //this will print the object as React.createElement()  this is an object not an html tag
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);                    
